f1817cc64c3086de248befd182a9dc6d50b99a7f
João Vitor <joao1301moura@gmail.com>
2025-03-14 15:41:02 -0300

    Corrigi algumas coisas na leitura de código em JS

--- code.js
+++ code.js
@@ -1,25 +1,23 @@
 const insta = document.getElementById("ig");
+const github = document.getElementById("ghub");
+const tiktok = document.getElementById("tik");
+const youtube = document.getElementById("tube");
 
 insta.addEventListener("click", function() {
     window.location.href = "https://www.instagram.com/ojaomoura/", "_blank";
 
 });
 
-const github = document.getElementById("ghub");
 
 github.addEventListener("click", function() {
     window.location.href = "https://github.com/JoaoVitordeMoura", "_blank";
 
 });
 
-const tiktok = document.getElementById("tik");
-
 tiktok.addEventListener("click", function() {
     window.location.href = "https://www.tiktok.com/@joaovitormoura13", "_blank";
 
 });
-
-const youtube = document.getElementById("tube");
 
 youtube.addEventListener("click", function() {
     window.location.href = "https://www.youtube.com/channel/UCzy21sF0WtdZ5nVqUXbRgIQ", "_blank";
