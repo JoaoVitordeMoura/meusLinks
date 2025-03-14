49ae74924bc1f6dc4757aa1f392e4634008e50d1
João Vitor <joao1301moura@gmail.com>
2025-03-14 14:24:21 -0300

    Salvei o meu projeto

---
+++ code.js
@@ -0,0 +1,28 @@
+const insta = document.getElementById("ig");
+
+insta.addEventListener("click", function() {
+    window.location.href = "https://www.instagram.com/ojaomoura/", "_blank";
+
+});
+
+const github = document.getElementById("ghub");
+
+github.addEventListener("click", function() {
+    window.location.href = "https://github.com/JoaoVitordeMoura", "_blank";
+
+});
+
+const tiktok = document.getElementById("tik");
+
+tiktok.addEventListener("click", function() {
+    window.location.href = "https://www.tiktok.com/@joaovitormoura13", "_blank";
+
+});
+
+const youtube = document.getElementById("tube");
+
+youtube.addEventListener("click", function() {
+    window.location.href = "https://www.youtube.com/channel/UCzy21sF0WtdZ5nVqUXbRgIQ", "_blank";
+
+});
+
